6ff5d05b0406fd8f06ae6d68322d6224bffbb06a
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-17 13:18:22 +0300

    chore: clean up index.js

--- growth-bot/index.js
+++
@@ -1,5 +0,0 @@
-import * as sdk from 'stacks-pix-sdk';
-
-console.log('Growth bot running for stacks-pix-sdk...');
-console.log('SDK exports:', Object.keys(sdk).join(', '));
-console.log('Smoke check passed.');
